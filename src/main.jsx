864e092ea5a8d55279c2574988c5fba50e538467
maren <jmcastri01@gmail.com>
2023-09-17 18:22:38 +0200

    cambio de hashrouter a browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp.jsx'
 
 import './styles.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store = { store }>
-      <HashRouter>
+      <BrowserRouter>
         <JournalApp />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
