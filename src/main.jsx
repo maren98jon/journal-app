e4e9810d3336b864deb405d5859f6890dba0e17b
maren <jmcastri01@gmail.com>
2023-09-17 09:58:30 +0200

    Build produccion

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp.jsx'
 
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store = { store }>
-      <BrowserRouter>
+      <HashRouter>
         <JournalApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
